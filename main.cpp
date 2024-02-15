b96dcfcd120ffa4a3f3fb84413e486d6af3c711f
vmnavarro94 <vm.navarro94@gmail.com>
2024-02-15 10:06:33 -0600

    Combat example

--- main.cpp
+++ main.cpp
@@ -3,9 +3,19 @@
 #include "Enemy/Enemy.h"
 
 int main() {
-    Player *player = new Player("Victor", 100, 10, 5, 10);
+    Player *player = new Player("Victor", 100, 8, 4, 10);
     Enemy *enemy = new Enemy("Estudiante de Redes", 50, 5, 2, 5, 10);
 
+    cout << player->toString() << endl;
+    cout << enemy->toString() << endl;
+
+    cout << "=====================" << endl;
+
+    player->doAttack(enemy);
+    enemy->doAttack(player);
+
+    cout << player->toString() << endl;
+    cout << enemy->toString() << endl;
 
     delete player;
     delete enemy;
